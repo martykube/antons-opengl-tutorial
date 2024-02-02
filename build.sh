14c723a82323badfcfc5458216a335de1adcd62f
Marty Kube <marty@beavercreekconsulting.com>
2024-02-01 20:07:39 -0500

    Update build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-g++ hello-triangle.cpp ./glew/lib/libGLEW.a ./glfw/build/src/libglfw3.a \
-    -I ./glfw/include -I glew/include/ \
+g++ hello-triangle.cpp ../glew/lib/libGLEW.a ../glfw/build/src/libglfw3.a \
+    -I ../glfw/include -I ../glew/include/ \
     -l GL -l X11 -l Xxf86vm -l Xrandr -l pthread -l Xi  -l Xinerama -l Xcursor \
     -l dl -l Xinerama
 
